4c9bb74db3d660eb3add614b10419e5346d967f6
Elliot Saba <staticfloat@gmail.com>
2025-03-02 02:26:58 +0000

    libLLVM v17.0.7

--- src/libLLVM_jll.jl
+++ src/libLLVM_jll.jl
@@ -1,10 +1,5 @@
-# Use baremodule to shave off a few KB from the serialized `.ji` file
-baremodule libLLVM_jll
-using Base
-using Base: UUID
-Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
-import JLLWrappers
+module libLLVM_jll
+using LazyJLLWrappers
+@generate_jll_from_toml()
+end # module libLLVM_jll
 
-JLLWrappers.@generate_main_file_header("libLLVM")
-JLLWrappers.@generate_main_file("libLLVM", UUID("8f36deef-c2a5-5394-99ed-8e07531fb29a"))
-end  # module libLLVM_jll
